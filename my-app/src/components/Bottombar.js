344f56f9b713340a098f53e119511c4e4cf4b44d
Rajin <rajink92@gmail.com>
2022-10-31 19:08:05 -0400

    base design

---
+++ my-app/src/components/Bottombar.js
@@ -0,0 +1,9 @@
+export default function bottombar() {
+  return (
+    <div className="h-24 bg-footer">
+      <div className="h-24 bg-footer border-t border-white border-opacity-5">
+        BottomBar Test
+      </div>
+    </div>
+  );
+}
